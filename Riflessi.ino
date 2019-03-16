3c41db4431f29902a0a277a6f54d85b96b98923e
Adrian <marculescu_adrian@libero.it>
2019-03-16 11:52:52 +0100

    Modifica tempo di rilessi con  funzioni e lcd

--- Riflessi.ino
+++ Riflessi.ino
@@ -1,39 +1,62 @@
-#define LED 8
-#define PULSANTE 6
-int premuto = 0;
-int tempoPrecedente = 0;
-int nuovoTempo = 0;
+#include <LiquidCrystal.h>
 
+#define LED 12
+#define PULSANTE 10
+int buzzerPin = 11;
+int premuto = 0;
+int tempoIniziale = 0;
+int tempoFinale = 0;
+LiquidCrystal lcd(2,3,4,5,6,7);
 void setup() {
    pinMode(LED, OUTPUT);
    pinMode(PULSANTE, INPUT);
    digitalWrite(LED, LOW);
-   Serial.begin(9600);
+   pinMode(7,OUTPUT);
    randomSeed(analogRead(0));
-
+   lcd.begin(16,2);
    
+   riflessiVisivi();
+   riflessiSonori();
 }
 
-void loop() {
-  Serial.println("Durata tempo attesa visivo");
-  while(Serial.available()==0) {} 
-  int tempoVisivo=random(2500,5000);
-  Serial.readString();
-  Serial.println(tempoVisivo);
-
-  
-   delay(tempoVisivo);
-   digitalWrite(LED, HIGH);
-   tempoPrecedente = millis();
-   while (premuto == 0) {
-     premuto = digitalRead(PULSANTE);
+void riflessiVisivi(){
+  int tempoVisivo=random(2000,5000);
+  delay(tempoVisivo);
+  digitalWrite(LED, HIGH);
+  tempoIniziale = millis();
+  while (premuto == 0) {
+    premuto = digitalRead(PULSANTE);
      if (premuto == HIGH) {
-       nuovoTempo = millis();
+       tempoFinale = millis();
        digitalWrite(LED, LOW);
      }
    }
    premuto = 0;
-   Serial.print("Tempo (ms): ");
-   Serial.println(nuovoTempo - tempoPrecedente);
+   lcd.setCursor(0,0);
+  int ciao= tempoFinale - tempoIniziale;
+   lcd.print(ciao);
+}
+void riflessiSonori(){
+  int tempoSonoro=random(2000, 5000);
+  delay(tempoSonoro);
+   tone(7, 300,5000);
+   tempoIniziale=millis();
+   while(premuto==0){
+    premuto = digitalRead(PULSANTE);
+     if (premuto == HIGH) {
+       tempoFinale = millis();
+       noTone(buzzerPin);
+     }
+   }
+   premuto = 0;
+   lcd.setCursor(0,1);
+   int ciao= tempoFinale - tempoIniziale;
+   lcd.print(ciao);
+} 
 
+
+
+
+
+void loop() {
 }
